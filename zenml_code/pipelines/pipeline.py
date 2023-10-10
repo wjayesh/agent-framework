b2ab6a63f344ba2f55721422616bd200ba0b654d
Jayesh Sharma <wjayesh@outlook.com>
2023-10-10 21:54:17 +0530

    also track agent in pipeline

--- zenml_code/pipelines/pipeline.py
+++ zenml_code/pipelines/pipeline.py
@@ -3,11 +3,12 @@
 from typing import Dict, List
 from zenml import pipeline
 
-from agent.agent import URL
+from agent.agent import URL, Agent
 from steps.url_scraper import url_scraper
 from steps.web_url_loader import web_url_loader
 from steps.index_generator import index_generator
 from steps.get_tools import get_tools
+from steps.get_agent import get_agent
 
 PIPELINE_NAME = "index_creation_pipeline"
 
@@ -15,12 +16,14 @@ PIPELINE_NAME = "index_creation_pipeline"
 def index_creation_pipeline(
     project_name: str,
     urls: Dict[str, List[URL]],
+    agent: Agent
 ) -> None:
     """Pipeline to create the index for the agent to use.
 
     Args:
         project_name: name of the project
         urls: dictionary with version as key and list of URLs as value
+        agent: The agent object that triggered this pipeline.
     """
     # create scrapable and non-scrapable URLs maps
     # depending on the value of url.scrape
@@ -41,6 +44,11 @@ def index_creation_pipeline(
         scraped_urls[version].extend(non_scrapable_urls[version])
     documents = web_url_loader(scraped_urls)
     vector_stores = index_generator(documents)
+    # TODO the last step should be get agent
+    # which will take all the tools from the previous agent
+    # and create a new agent based on the values of the current agent
+    # values being the prompt that is being used, etc.
     all_tools = get_tools(project_name, vector_stores, scraped_urls)
+    agent = get_agent(agent)
 
-    return all_tools
+    return agent
